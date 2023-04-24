84cd1735808e2ba74fec90626afcddb1453bb21c
Documenter.jl <documenter@juliadocs.github.io>
2023-04-24 20:52:44 +0000

    build based on 6ddff63

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PyDraw","category":"page"},{"location":"#PyDraw","page":"Home","title":"PyDraw","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PyDraw.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PyDraw]","category":"page"}]
+}
